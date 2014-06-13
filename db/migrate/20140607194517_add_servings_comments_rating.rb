1f6f8f4935ec6592a02195d0ec6eb0db43f1b2b9
Giovanni <giovanni.delacqua@gmail.com>
2014-06-13 10:41:52 -0500

    Delete 20140607194517_add_servings_comments_rating.rb

--- db/migrate/20140607194517_add_servings_comments_rating.rb
+++
@@ -1,8 +0,0 @@
-class AddServingsCommentsRating < ActiveRecord::Migration
-  def change
-    rename_column :recipes, :description, :ingredients
-    add_column :recipes, :servings, :integer, :default => 2
-    add_column :recipes, :comment, :string
-    add_column :recipes, :rating, :integer
-  end
-end
