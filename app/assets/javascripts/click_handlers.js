89fae4e78909c204bed65435564085414752439a
Giovanni Delacqua <giovanni.delacqua@gmail.com>
2014-06-20 08:10:13 -0500

    comments slider added to show ajax comments count added to show ajax

--- app/assets/javascripts/click_handlers.js
+++ app/assets/javascripts/click_handlers.js
@@ -9,10 +9,20 @@
     //$('#wrap').hide();
     //$('#hunter').click(showSecondo);
     $('#recipe_wrap').on('click', '.close_recipe', close_recipe);
+    $('#recipe_wrap').on('click', '.comments', showComments);
+    $('#recipe_wrap').on('click', '.close_comments', closeComments);
   }
 
   function close_recipe(){
     $('#recipe_wrap').fadeOut(500);
+  }
+
+  function showComments(){
+    $('.r_all_comments').animate({left: '-420px'},1000);
+  }
+
+  function closeComments(){
+    $('.r_all_comments').animate({left: '-850px'},1000);
   }
 
 
